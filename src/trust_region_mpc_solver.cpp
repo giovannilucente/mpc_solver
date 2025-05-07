67b849bed352202d4491366d08bc51385cfdd5a7
luce_gi <giovanni.lucente@dlr.de>
2025-05-07 14:55:32 +0200

    reduced maximum number of iterations

--- src/trust_region_mpc_solver.cpp
+++ src/trust_region_mpc_solver.cpp
@@ -555,7 +555,7 @@ void TrustRegionMPCSolver::trust_region_solver(double* U_)
     double r_ = 1e-8;
     double threshold_gradient_norm = 1e-2;
     int iter = 1;
-    int iter_lim = 200;
+    int iter_lim = 20;
 
     // Variables definition:
     double gradient[nu];
